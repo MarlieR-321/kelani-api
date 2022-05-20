81af96aa2d00027727e1e61c4854c1c7def54aa1
SJAR03 <sayerdisrodriguez@gmail.com>
2022-05-19 23:10:46 -0500

    Habia olvidado los FinById de producto, ahora si listo, creo

--- src/main/java/uca/edu/ni/kelani/controllers/ProductoController.java
+++ src/main/java/uca/edu/ni/kelani/controllers/ProductoController.java
@@ -1,9 +1,13 @@
 package uca.edu.ni.kelani.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,9 +22,19 @@ public class ProductoController {
 	@Autowired
 	ProductoServices proS;
 	
+	@GetMapping(path="/listar")
+	public List<Producto> listar() {
+		return proS.ListarRegistro();
+	}
+	
 	@PostMapping(path = "/add")
 	public Producto insert(@RequestBody Producto p) {
 		return proS.GuardarRegistro(p);
+	}
+	
+	@PutMapping(path="/edit/{id}")
+	public Producto edit(@RequestBody Producto p) {
+		return proS.EditarRegistro(p);
 	}
 
 	@DeleteMapping(path = "/delete/{id}")
--- src/main/java/uca/edu/ni/kelani/services/ProductoServices.java
+++ src/main/java/uca/edu/ni/kelani/services/ProductoServices.java
@@ -1,8 +1,8 @@
 package uca.edu.ni.kelani.services;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import uca.edu.ni.kelani.modelos.Producto;
 import uca.edu.ni.kelani.repositories.ProductoRepository;
 
@@ -12,7 +12,16 @@ public class ProductoServices{
 	@Autowired(required = true)
 	ProductoRepository prp;
 
+	public List<Producto> ListarRegistro() {
+		// TODO Auto-generated method stub
+		return prp.findAll();
+	}
+	
 	public Producto GuardarRegistro(Producto p) {
+		return prp.save(p);
+	}
+	
+	public Producto EditarRegistro(Producto p) {
 		return prp.save(p);
 	}
 	
